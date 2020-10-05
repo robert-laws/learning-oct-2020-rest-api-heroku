57083a6d19be73cbfd55fbe2101ef8406c24d349
Robert Laws <roblaws@me.com>
2020-10-05 16:24:51 +0300

    set up post endpoint for books

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
+const booksRoute = require('./routes/books');
 
 const PORT = process.env.PORT || 3000;
 
-// connect to mongodb atlas
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// routes
+app.use('/api/books', booksRoute);
+
+// connect to mongodb atlas
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -14,6 +21,9 @@ mongoose
   })
   .then(() => {
     console.log('connected to mongodb atlas');
+  })
+  .catch((error) => {
+    console.log('Error in app', error);
   });
 
 app.listen(PORT, () => {
